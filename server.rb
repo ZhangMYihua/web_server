b30078da7bc1cc9134f558d03a59cf6607655f88
zhangmyihua <michaelzhang2@msn.com>
2015-01-26 15:15:36 -0500

    Created lines to be read by the server

--- server.rb
+++ server.rb
@@ -7,18 +7,26 @@ port = 2000
 server = TCPServer.open(host, port)                 # Socket to listen to defined host and port
 puts "Server started on #{host}:#{port} ..."        # Output to stdout that server started
 
-filename = "index.html"
-response = File.read(filename)
-
 loop do                                             # Server runs forever
   client = server.accept                            # Wait for a client to connect. Accept returns a TCPSocket
 
   lines = []
-  while (line = client.gets.chomp) && !line.empty?  # Read the request and collect it until it's empty
-    lines << line
+  while (line = client.gets.chomp)   # Read the request and collect it until it's empty
+    break if line.empty?
+	lines << line
   end
-  puts lines                                        # Output the full request to stdout
 
-  client.puts(response)                       # Output the current time to the client
+
+  puts lines            
+
+  filename = lines[0].gsub(/GET \//, '').gsub(/\ HTTP.*/, '')
+	response_body = nil
+	if File.exists?(filename)
+	  response_body = File.read(filename)
+	else
+	  response_body = "File Not Found\n" 				# need to indicate end of the string with \n
+	end                           						 # Output the full request to stdout
+
+  client.puts(response_body)                       # Output the current time to the client
   client.close                                      # Disconnect from the client
 end

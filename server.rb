be88b467d1a59109a98668ee986458e36a89a3a3
zhangmyihua <michaelzhang2@msn.com>
2015-01-27 09:39:13 -0500

    Moved functions into methods

--- server.rb
+++ server.rb
@@ -7,19 +7,44 @@ port = 2000
 server = TCPServer.open(host, port)                 # Socket to listen to defined host and port
 puts "Server started on #{host}:#{port} ..."        # Output to stdout that server started
 
-loop do                                             # Server runs forever
-  client = server.accept                            # Wait for a client to connect. Accept returns a TCPSocket
+
+def read_request(client)
 
   lines = []
+
   while (line = client.gets.chomp)  				# Read the request and collect it until it's empty
     break if line.empty?
 	lines << line
   end
+   return lines 
+end
 
+def file_type(filename)
+  		if filename =~ /.css/
+	  		content_type = "text/css"
+	  	elsif filename =~ /.html/
+	  		content_type = "text/html"
+	  	else 
+	  		content_type = "text/plain"
+	  	end
+end 
 
-  puts lines.inspect            
+def parse_filename(request)
+		parsed_file_name = request[0].gsub(/GET \//, '').gsub(/\ HTTP.*/, '')
+		return parsed_file_name
+end
 
-  filename = lines[0].gsub(/GET \//, '').gsub(/\ HTTP.*/, '')
+loop do
+		
+	client = server.accept
+
+	request = read_request(client)
+
+  puts request         
+
+  filename = parse_filename(request)
+	
+
 	response_body = nil
 	headers = []
 
@@ -27,14 +52,7 @@ loop do                                             # Server runs forever
 	if File.exists?(filename)
 	  response_body = File.read(filename)
 	  	headers << "HTTP/1.1 200 OK"
-
-	  	if filename =~ /.css/
-	  		content_type = "text/css"
-	  	elsif filename =~ /.html/
-	  		content_type = "text/html"
-	  	else 
-	  		content_type = "text/plain"
-	  	end
+	  	content_type = file_type(filename)
 		headers << "Content type: #{content_type}"
 	else
 	  response_body = "File Not Found\n" 
